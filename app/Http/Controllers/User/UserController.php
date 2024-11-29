1e789be0b962084a8bed83b4f0c1ef5abf6e06c3
Mu'adz Mohd Rosli <muadz.mrosli@gmail.com>
2024-11-29 15:01:22 +0800

    refactor: fix code style

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
     public function index(Request $request): JsonResponse
     {
         $users = User::filter($request->all())->get();
+
         return $this->apiResponse->success(UserListingResource::collection($users));
     }
 
